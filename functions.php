a71fedfc7cd68718cf17f2874635df3799b66da6
Charles J Dillon <charles.dillon2@wsu.edu>
2014-06-12 14:58:16 -0700

    Remove filter add parts
    Remove non essential filter from functions file
    Add parts file

--- functions.php
+++ functions.php
@@ -1,8 +1,4 @@
 <?php
-add_action( 'spine_pre_jacket_html', 'ucomm_add_banner_html' );
-function ucomm_add_banner_html() {
-       echo '<div class="acBanner"></div>';
-}
 add_theme_support( 'post-thumbnails' ); 
 add_filter( 'spine_enable_builder_module', '__return_true' );
 
