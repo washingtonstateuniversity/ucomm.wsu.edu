ad12e2e25c119c92700e234282a2823c6f611c6a
Charles J Dillon <charles.dillon2@wsu.edu>
2014-06-24 23:13:59 -0700

    Moves the homepage to page builder
    Moves homepage to page builder and adds fluid video

--- functions.php
+++ functions.php
@@ -28,4 +28,12 @@ function ucomm_the_excerpt( $excerpt ) {
 
 	return $excerpt;
 }
+/* 
+Add responsive container to embeds
+------------------------------------ */ 
+function alx_embed_html( $html ) {
+    return '<div class="video-container">' . $html . '</div>';
+}
+add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'alx_embed_html' ); // Jetpack
 ?>
