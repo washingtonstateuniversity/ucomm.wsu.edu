c03437908339f336a19b504e547293288263b75d
Charles J Dillon <charles.dillon2@wsu.edu>
2014-07-09 16:36:14 -0700

    Add back video container wrap

--- functions.php
+++ functions.php
@@ -39,7 +39,14 @@ function ucomm_the_excerpt( $excerpt ) {
 
 	return $excerpt;
 }
-
+/* 
+Add responsive container to embeds
+------------------------------------ */ 
+function alx_embed_html($html) {
+    return '<div class="video-container">'.$html.'</div>';
+}
+add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'alx_embed_html' ); // Jetpack
 
 add_filter( 'tablepress_datatables_parameters', 'spine_params', 10, 4 );
 /**
