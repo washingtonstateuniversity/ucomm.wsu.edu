ba564316d8002c294c16f8b3e63817553d8a59f8
jeremyfelt <jeremy.felt@gmail.com>
2014-07-09 10:53:05 -0700

    Attach the click event on #fixedSide and look anchor to bubble up
    This saves on the number of click events added to the document

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 (function($){
 	$( ".mainAccordion" ).accordion({ heightStyle: "content" });
 
-	$('a[href*=#]:not([href=#])').click(function() {
+	$('#fixedSide' ).on('click','a[href*=#]:not([href=#])', function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
