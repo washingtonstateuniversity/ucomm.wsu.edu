15098fef2c56e3e8606df401510ea6871d360343
Charles J Dillon <charles.dillon2@wsu.edu>
2014-07-11 10:46:49 -0700

    Change the video container to fluid
    So that the class makes sense on maps as well as video, I have changed
    the class name.

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function ucomm_the_excerpt( $excerpt ) {
 Add responsive container to embeds
 ------------------------------------ */ 
 function alx_embed_html($html) {
-    return '<div class="video-container">'.$html.'</div>';
+    return '<div class="fluid-container">'.$html.'</div>';
 }
 add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
 add_filter( 'video_embed_html', 'alx_embed_html' ); // Jetpack
