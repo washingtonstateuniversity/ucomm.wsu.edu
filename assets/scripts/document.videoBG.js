e9766ee819d7140f1e269f6c89e1b23dd8721406
Charles J Dillon <charles.dillon2@wsu.edu>
2014-07-17 17:04:49 -0700

    Fix header outside of 100%

---
+++ assets/scripts/document.videoBG.js
@@ -0,0 +1,9 @@
+	$(document).ready(function() {
+		$('#video-background').videoBG({
+			mp4:'/wp-content/uploads/sites/61/2014/07/banner.mp4',
+			ogv:'assets/banner.ogv',
+			webm:'assets/banner.webm',
+			poster:'/wp-content/uploads/sites/61/2014/07/banner.jpg',
+			scale:true,
+			zIndex:0
+	});	
