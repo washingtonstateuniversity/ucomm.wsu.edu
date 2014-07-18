ede0ec543d33908cc375c94c11b40613930a24b1
Charles J Dillon <charles.dillon2@wsu.edu>
2014-07-18 09:04:31 -0700

    General style changes and file paths for videos

--- assets/scripts/document.videoBG.js
+++ assets/scripts/document.videoBG.js
@@ -1,8 +1,8 @@
 	$(document).ready(function() {
 		$('#video-background').videoBG({
 			mp4:'/wp-content/uploads/sites/61/2014/07/banner.mp4',
-			ogv:'assets/banner.ogv',
-			webm:'assets/banner.webm',
+			ogv:'/wp-content/uploads/sites/61/2014/07/banner.ogv',
+			webm:'/wp-content/uploads/sites/61/2014/07/banner.webm',
 			poster:'/wp-content/uploads/sites/61/2014/07/banner.jpg',
 			scale:true,
 			zIndex:0
