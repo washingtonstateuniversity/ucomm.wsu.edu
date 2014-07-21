f1fe5170c48bed3445c7d9c498348d7d7bbca4c5
jeremyfelt <jeremy.felt@gmail.com>
2014-07-21 15:00:04 -0700

    Remove video background scripting in favor of new plugin

--- assets/scripts/document.videoBG.js
+++
@@ -1,9 +0,0 @@
-	$(document).ready(function() {
-		$('#video-background').videoBG({
-			mp4:'/wp-content/uploads/sites/61/2014/07/banner.mp4',
-			ogv:'/wp-content/uploads/sites/61/2014/07/banner.ogv',
-			webm:'/wp-content/uploads/sites/61/2014/07/banner.webm',
-			poster:'/wp-content/uploads/sites/61/2014/07/banner.jpg',
-			scale:true,
-			zIndex:0
-	});	
