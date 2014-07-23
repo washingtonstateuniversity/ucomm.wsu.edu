dd4fa739e4cd19ea030f8af5e42c170fd68a63bb
jeremyfelt <jeremy.felt@gmail.com>
2014-07-23 08:09:43 -0700

    Move ucomm_library shortcode to ucomm-shortcodes

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 add_filter( 'spine_enable_builder_module', '__return_true' );
 
-include_once( 'includes/library-of-work.php' ); // Handles library of work shortcode
 include_once( 'includes/web-template.php' );
 include_once( 'includes/ucomm-shortcodes.php' ); // Handle custom shortcodes for Ucomm.
 
