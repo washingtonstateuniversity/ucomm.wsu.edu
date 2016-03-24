d4b452db571598f259b9ae2cda30df55f1083d1d
Jeremy Felt <jeremy.felt@gmail.com>
2016-03-24 13:43:26 -0700

    Remove the sidebar from the WooCommerce template

--- woocommerce.php
+++ woocommerce.php
@@ -3,21 +3,14 @@
 	<main class="spine-page-default">
 
 		<?php get_template_part( 'parts/headers' ); ?>
-		<?php get_template_part( 'parts/featured-images' ); ?>
 
-		<section class="row side-right gutter pad-ends">
+		<section class="row single gutter pad-ends">
 
 			<div class="column one">
 
 				<?php woocommerce_content(); ?>
 
 			</div><!--/column-->
-
-			<div class="column two">
-
-				<?php get_sidebar(); ?>
-
-			</div><!--/column two-->
 
 		</section>
 
