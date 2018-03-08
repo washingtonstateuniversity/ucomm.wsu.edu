b5e5244ea28183d0813111c5b98ddcc1082b8558
Jeremy Felt <jeremy.felt@gmail.com>
2018-03-08 14:54:40 -0800

    Fix JSCS issues with autofix

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,4 +1,4 @@
-(function($){
+( function( $ ) {
 
 	function display_modal_data( data ) {
 		var html = '<div class="content-modal"><div class="content-modal-content"><div class="content-modal-close"><span class="ss-icon">Close</span></div>' +
@@ -7,19 +7,19 @@
 			data.title.rendered +
 			'</h1></header><div class="article-body">' +
 			data.content.rendered +
-			'</div></article>' +
-			'</div></section></div></div>';
-		$('main' ).append(html);
-		$('.content-modal-close' ).on('click', function(e) {
-			$('.content-modal' ).remove();
-		});
+			"</div></article>" +
+			"</div></section></div></div>";
+		$( "main" ).append( html );
+		$( ".content-modal-close" ).on( "click", function( e ) {
+			$( ".content-modal" ).remove();
+		} );
 	}
 
-	function pull_modal_data(e) {
+	function pull_modal_data( e ) {
 		e.preventDefault();
-		var post_id = $(this).attr('data-post-id');
-		var post_url = window.UComm_Data.json_api_url + 'posts/' + post_id;
+		var post_id = $( this ).attr( "data-post-id" );
+		var post_url = window.UComm_Data.json_api_url + "posts/" + post_id;
 		$.getJSON( post_url, display_modal_data );
 	}
-	$('.library-nav' ).on('click','a', pull_modal_data );
-}(jQuery));
+	$( ".library-nav" ).on( "click", "a", pull_modal_data );
+}( jQuery ) );
