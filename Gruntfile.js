dea4f22ea868df1783f381c2a53b627ed227454b
Jeremy Felt <jeremy.felt@gmail.com>
2018-03-08 14:53:35 -0800

    Add theme skeleton structure

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		stylelint: {
+			src: [ "css/*.css" ]
+		},
+
+		concat: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				src: "css/*.css",
+				dest: "tmp-style.css"
+			}
+		},
+
+		postcss: {
+			options: {
+				map: true,
+				diff: false,
+				processors: [
+					require( "autoprefixer" )( {
+						browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
+					} )
+				]
+			},
+			dist: {
+				src: "tmp-style.css",
+				dest: "style.css"
+			}
+		},
+
+		clean: {
+			options: {
+				force: true
+			},
+			temp: [ "tmp-style.css", "tmp-style.css.map" ]
+		},
+
+		jscs: {
+			scripts: {
+				src: [ "Gruntfile.js", "assets/scripts/*.js" ],
+				options: {
+					preset: "jquery",
+					requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+					maximumLineLength: 250,
+					validateIndentation: "\t"
+				}
+			}
+		},
+
+		jshint: {
+			grunt_script: {
+				src: [ "Gruntfile.js" ],
+				options: {
+					curly: true,
+					eqeqeq: true,
+					noarg: true,
+					quotmark: "double",
+					undef: true,
+					unused: false,
+					node: true     // Define globals available when running in Node.
+				}
+			},
+			theme_scripts: {
+				src: [ "assets/scripts/*.js" ],
+				options: {
+					bitwise: true,
+					curly: true,
+					eqeqeq: true,
+					forin: true,
+					freeze: true,
+					noarg: true,
+					nonbsp: true,
+					quotmark: "double",
+					undef: true,
+					unused: true,
+					browser: true, // Define globals exposed by modern browsers.
+					jquery: true   // Define globals exposed by jQuery.
+				}
+			}
+		},
+
+		phpcs: {
+			plugin: {
+				src: "./"
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
+			}
+		}
+
+	} );
+
+	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-connect" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-stylelint" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+
+	// Default task(s).
+	grunt.registerTask( "default", [ "jscs", "jshint", "stylelint", "concat", "postcss", "clean" ] );
+};
